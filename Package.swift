a1b48da7e91af59fef0c5a24ad7c0c7156d2e715
Trevor Sheridan <trevorsheridan@users.noreply.github.com>
2025-09-05 08:24:26 -0600

    Update AsyncReactiveSequences

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.1"),
         .package(url: "https://github.com/groue/Semaphore.git", from: "0.1.0"),
-        .package(url: "https://github.com/trevorsheridan/AsyncReactiveSequences.git", branch: "main")
+        .package(url: "https://github.com/trevorsheridan/AsyncReactiveSequences.git", branch: "trevor/ios26")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
