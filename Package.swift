904fc7fce38192e3e8ff3429283ed6e8f4d5b6e6
Trevor Sheridan <trevorsheridan@users.noreply.github.com>
2025-09-07 20:16:13 -0600

    Lower swift tools to 6.0 and dependency on AsyncReactiveSequences to 0.1.0 since this code will continue to work with that version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.1"),
         .package(url: "https://github.com/groue/Semaphore.git", from: "0.1.0"),
-        .package(url: "https://github.com/trevorsheridan/AsyncReactiveSequences.git", .upToNextMinor(from: "0.2.0"))
+        .package(url: "https://github.com/trevorsheridan/AsyncReactiveSequences.git", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
